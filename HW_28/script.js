a7cc6f5522a65ab6c586bd3c823791de45ddb35c
Mila_Star <101258091+MilaStar22@users.noreply.github.com>
2023-05-30 18:53:35 +0200

    add homework_28: jQuery

---
+++ HW_28/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+  $('.content').slick({
+    infinite: true,
+  });
+});
+
+
+$('.slider-nav').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  dots: true,
+  centerMode: true,
+  focusOnSelect: true
+});
+
+$('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav'
+});
+
